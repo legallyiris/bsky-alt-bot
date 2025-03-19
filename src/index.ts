d3d1effbf42f50467912f93af1bbfd8f7f4a268e
iris <iris@heyiris.dev>
2025-03-19 18:40:31 +0000

    feat: login and config stuff

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+import { Bot } from "@skyware/bot";
+import { config } from "./config";
+
+async function main() {
+	const bot = new Bot({ service: config.atProto.pds });
+
+	try {
+		console.log(`logging in as ${config.atProto.identifier}`);
+		await bot.login({
+			identifier: config.atProto.identifier,
+			password: config.atProto.password,
+		});
+		console.log(`logged in as ${bot.agent.handle}`);
+	} catch (error) {
+		console.error("error logging in", error);
+		process.exit(1);
+	}
+
+	bot.on("mention", async (mention) => {
+		await mention.reply({ text: "miaow ! " });
+	});
+}
+
+main().catch((error) => {
+	console.error(error);
+});
